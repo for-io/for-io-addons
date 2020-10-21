f667f18d076595efb4565bd0b64c2c3f6ea225d2
Ilija Trajkoski <ile2807@gmail.com>
2020-10-21 17:53:47 +0200

    Presign for putting objects added

---
+++ aws/test/mocks/preSignMock.js
@@ -0,0 +1,10 @@
+exports['MOCK presign'] = {
+
+    async preSign() {
+        return { 
+            success: true,
+            url: "https://amazon.com/testMock?uid=098213948234987239487239487234"
+        };
+    },
+
+};
